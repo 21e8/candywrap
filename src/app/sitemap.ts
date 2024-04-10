f7809594c62715306ef242e66c4c4d7308a9eb76
21e8 <alize_in_cryptoland@protonmail.com>
2024-04-10 09:06:28 +0200

    feat: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,11 @@
 import { MetadataRoute } from "next";
 import { CHAIN_IDS, SUPPORTED_CHAINS } from "./_constants";
-
+const HOST = "https://candywrap.dev";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "/", lastModified: new Date() },
+    { url: `${HOST}`, lastModified: new Date() },
     ...SUPPORTED_CHAINS.map((chain) => ({
-      url: `/${CHAIN_IDS[chain.id]}`,
+      url: `${HOST}/${CHAIN_IDS[chain.id]}`,
       lastModified: new Date(),
     })),
   ];
