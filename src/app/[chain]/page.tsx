956d26ada7258ffb1034a9b6cbad4ee7c7d77f8a
21e8 <alize_in_cryptoland@protonmail.com>
2024-04-10 20:12:40 +0200

    feat: small stuff

--- src/app/[chain]/page.tsx
+++ src/app/[chain]/page.tsx
@@ -1,16 +1,17 @@
 import Ui from "../_ui";
 import { CHAINS, IDS, SYMBOLS } from "../_constants";
-import Image from 'next/image';
 import { Logo } from "@/components/logo";
+
 export default function Home({ params }: { params: { chain: string } }) {
   const actualSymbol = SYMBOLS[IDS[params.chain]] || "ETH";
   return (
     <>
-      <div className="my-12 mx-12 text-secondary">
+      <div className="my-12 lg:mx-12 text-secondary">
         <Logo />
       </div>
       <h1 className="text-2xl mt-4 lg:mt-8 mb-6 lg:mb-12 text-center hidden">
-        Wrap/Unwrap {actualSymbol} on <span> {CHAINS[params.chain]?.name}</span>
+        Wrap/Unwrap {actualSymbol}/ w{actualSymbol} on{" "}
+        <span> {CHAINS[params.chain]?.name}</span>
       </h1>
       <Ui chain={params.chain} />
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { Logo } from "@/components/logo";
 import Ui from "./_ui";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center px-8 py-6 max-w-7xl mx-auto">
-      <h1 className="text-3xl mb-4">Candywrap</h1>
-      <h1 className="text-2xl mb-12">Wrap/Unwrap ETH/wETH</h1>
+      <div className="my-12 lg:mx-12 text-secondary">
+        <Logo />
+      </div>
+      <h1 className="text-2xl mt-4 lg:mt-8 mb-6 lg:mb-12 text-center hidden">
+        Wrap/Unwrap ETH/ wETH on <span> Ethereum</span>
+      </h1>
       <Ui chain="ethereum" />
     </main>
   );
