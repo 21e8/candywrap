7336dd5939e9fa81cda622f17fdba7c17301b8bf
21e8 <alize_in_cryptoland@protonmail.com>
2024-04-10 19:09:17 +0200

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./_providers";
 import { BASE_METADATA } from "./_base-metadata";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark`}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
